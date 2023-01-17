fdc6064c415285f44ff7942606f401b10b9fcb09
Mohamed D. Ahmed <Mohamed@Mohameds-MBP-3.fios-router.home>
2023-01-17 16:26:35 -0500

    formating

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Setup Initialization (from pull): 
+// Setup Initialization (from pull):
 // 0. delete all files except server.js, set up git upstream in "backend" folder
 // 1. npm init (creates package.json)
 // 2. npm i dotenv mongoose express cors morgan (creates node modules)
@@ -22,99 +22,99 @@
 // 3. git checkout -b development (switch from local master to local development branch)
 // begin more changes
 
-// ------- (if needed) git merge master - (combines master to development) 
+// ------- (if needed) git merge master - (combines master to development)
 
 // ----------------------------------------------------------------------------------------------
 // Dependencies----------
-require('dotenv').config(); // get .env variables
-const express = require('express'); // import express
+require("dotenv").config(); // get .env variables
+const express = require("express"); // import express
 const mongoose = require("mongoose"); // import mongoose
 const morgan = require("morgan"); // import morgan
-const cors = require('cors') // import cors 
+const cors = require("cors"); // import cors
 
 // Initialize the Express App----------
-const app = express() // create application object
+const app = express(); // create application object
 
 // Configure App Settings----------
-const {PORT = 4000, MONGODB_URL} = process.env; // pull PORT and MONGODB from .env, give default value of 4000
+const { PORT = 4000, MONGODB_URL } = process.env; // pull PORT and MONGODB from .env, give default value of 4000
 
 // DATABASE CONNECTION----------------
 mongoose.connect(MONGODB_URL); // connect to mongoDB
 
 mongoose.connection // mongo status listeners
-    .on("open", () => console.log("Connected to MongoDB"))
-    .on("error", (error) => console.log('Error with MongoDB:' + error.message));
+  .on("open", () => console.log("Connected to MongoDB"))
+  .on("error", (error) => console.log("Error with MongoDB:" + error.message));
 
 // Mount Middleware----------
-app.use(cors()) // to prevent cors errors, open access to all origins
-app.use(morgan("dev")) // logging
-app.use(express.json()) // creates "req.body"
+app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(morgan("dev")); // logging
+app.use(express.json()); // creates "req.body"
 // do not use app.use(express.urlencoded) (ONLY FOR Express serving HTML not JSON)
- 
+
 // MODELS (Set up model)----------------
-const itemSchema = new mongoose.Schema({
+const itemSchema = new mongoose.Schema(
+  {
     name: String,
     image: String,
     description: String,
-    price: Number
-}, {timestamps: true})
+    price: Number,
+  },
+  { timestamps: true }
+);
 
-const Item = mongoose.model('Item', itemSchema)
+const Item = mongoose.model("Item", itemSchema);
 
 // ROUTES--------------------------------------------------
 app.get("/", (req, res) => {
-    res.send("hello world");
+  res.send("hello world");
 });
 
 // Index
-app.get('/rummage', async (req,res) => { // New, shorter syntax using async await
+app.get("/rummage", async (req, res) => {
+  // New, shorter syntax using async await
 
-    // Try/catch statement catches error before it crashes program
-    try{ 
-        const item = await Item.find({}); // Instead of People.find({}, (err, people))
-        res.json(item); 
-    } catch (error) {
-        console.log('error: ', error);
-        res.json({error: 'something went wrong - check console'})
-    }
-})
+  // Try/catch statement catches error before it crashes program
+  try {
+    const item = await Item.find({}); // Instead of People.find({}, (err, people))
+    res.json(item);
+  } catch (error) {
+    console.log("error: ", error);
+    res.json({ error: "something went wrong - check console" });
+  }
+});
 
 // Create
-app.post('/rummage', async (req, res) => {
-    try {
-        const item = await Item.create(req.body);
-        res.json(item);
-    } catch (error) {
-        console.log('error: ', error);
-        res.json({error: 'something went wrong - check console'})
-    }
-})
+app.post("/rummage", async (req, res) => {
+  try {
+    const item = await Item.create(req.body);
+    res.json(item);
+  } catch (error) {
+    console.log("error: ", error);
+    res.json({ error: "something went wrong - check console" });
+  }
+});
 
 // Delete
-app.delete('/rummage/:id', async (req, res) => {
-    try {
-        res.json(await Item.findByIdAndDelete(req.params.id));
-    } catch (error) {
-        console.log('error: ', error);
-        res.json({error: 'something went wrong - check console'})
-    }
-})
+app.delete("/rummage/:id", async (req, res) => {
+  try {
+    res.json(await Item.findByIdAndDelete(req.params.id));
+  } catch (error) {
+    console.log("error: ", error);
+    res.json({ error: "something went wrong - check console" });
+  }
+});
 
 // Update
-app.put('/rummage/:id', async (req, res) => {
-    try {
-        res.json(await Item.findByIdAndUpdate(
-            req.params.id, 
-            req.body, 
-            {new: true} 
-        ));
-    } catch (error){
-        console.log('error: ', error)
-        res.json({error: 'something went wrong - check console'});
-    }
-})
+app.put("/rummage/:id", async (req, res) => {
+  try {
+    res.json(
+      await Item.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (error) {
+    console.log("error: ", error);
+    res.json({ error: "something went wrong - check console" });
+  }
+});
 
 // LISTENER---------- (Tell Express to Listen)
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
-
-
